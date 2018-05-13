49c279d7341a7d1b2418869e2a36910fa7f89778
Mark McPherson <msm1089@yahoo.co.uk>
2018-05-13 20:26:50 +0100

    hilo-game-1 Add basic code

--- program.py
+++ program.py
@@ -1,11 +1,31 @@
 import random
 
 
-secret_number = random.randint(1, 100)
-
-
 def main():
+    play = True
+
+    while play:
+        game_loop()
+
+        print("Press 'y' to play again, or any other key to exit.")
+        play_again = input()
+        play = play_again in ('y', 'Y')
+
+
+def game_loop():
+    secret_number = random.randint(1, 100)
     print("Guess the number!")
+
+    while True:
+        input_number = input()
+
+        if input_number < secret_number:
+            print("That is too low.")
+        elif input_number > secret_number:
+            print("That is too high.")
+        else:
+            print("You got it!!!")
+            break
 
 
 if __name__ == '__main__':
