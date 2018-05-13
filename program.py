126a2bcf93eb08460d5146d38ff5d6df670e810b
msm1089 <msm1089@yahoo.co.uk>
2018-05-13 21:12:08 +0100

    Feature/1 add user input (#9)
    * hilo-game-1 Add basic code
    
    * hilo-game-1 Add basic code
    
    * hilo-game-1 Add basic code
    
    * hilo-game-1 Add basic code

--- program.py
+++ program.py
@@ -1,11 +1,35 @@
 import random
 
 
-secret_number = random.randint(1, 100)
-
-
 def main():
+    play = True
+
+    while play:
+        game_loop()
+
+        print("Press 'y' to play again, or any other key to exit.")
+        play_again = input()
+        play = play_again in ('y', 'Y')
+
+
+def game_loop():
+    secret_number = get_secret_number()
     print("Guess the number!")
+
+    while True:
+        input_number = input()
+
+        if input_number < secret_number:
+            print("That is too low.")
+        elif input_number > secret_number:
+            print("That is too high.")
+        else:
+            print("You got it!!!")
+
+
+def get_secret_number():
+    secret_number = random.randint(1, 100)
+    return secret_number
 
 
 if __name__ == '__main__':
