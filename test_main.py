47223a9bb549f20c6b54e38eb4ed09db62b2a839
Mark McPherson <msm1089@yahoo.co.uk>
2018-05-13 21:08:39 +0100

    hilo-game-1 Add basic code

--- test_main.py
+++
@@ -1,7 +0,0 @@
-import program
-
-
-def test_secret_number_in_range():
-    secret_number = program.secret_number
-    print("The secret number is {}.".format(secret_number))
-    assert 1 <= secret_number & secret_number <= 100
