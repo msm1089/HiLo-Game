853ef244032c082f7507115deb2adef3b17e1722
Mark McPherson <msm1089@yahoo.co.uk>
2018-05-13 22:03:54 +0100

    added __init__.py file...fecking imports :(

--- test/test_main.py
+++ test/test_main.py
@@ -1,6 +1,6 @@
-import src.program
+from src.program import get_secret_number
 
 
 def test_secret_number_in_range():
-    secret_number = src.program.get_secret_number()
+    secret_number = get_secret_number()
     assert 1 <= secret_number <= 100
