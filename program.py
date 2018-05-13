1729b4a1969ab52fb18451a901f3aa22fe5ec4dd
msm1089 <msm1089@yahoo.co.uk>
2018-05-13 09:52:34 +0100

    tests added (#4)
    * added program.py
    
    * added config.yml
    
    * added (blank for now) requirements.txt to get CircleCI to build
    
    * added 's' to requirement.txt...
    
    * added tests
    
    * corrected path to test_main.py
    
    * corrected path to test_main.py
    
    * corrected path to test_main.py

--- program.py
+++ program.py
@@ -1,11 +1,11 @@
 import random
 
 
-def main():
-    SecretNumber = random.randint(1, 100)
+secret_number = random.randint(1, 100)
 
-    while True:
-        print("Guess the number!")
+
+def main():
+    print("Guess the number!")
 
 
 if __name__ == '__main__':
